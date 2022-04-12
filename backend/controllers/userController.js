9d2f8eefc30730f3ae4b9596017e63b9adf6ff74
GP <gregpetropoulos@yahoo.com>
2022-04-12 05:28:21 -0400

    Set up controller logic and custom middle ware to handle errors and status codes. Added the asyncHandler npm package and added the boy parser middleware to the server.js for urlencoding and sending raw json.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,32 @@
 // *ROUTE LOGIC
 //Logic use in place of the req,res for each route
 
-const registerUser = (req, res) => {
-  res.send('Register Route');
-};
+// Simple middleware for handling exceptions inside of async express routes and passing them to your express error handlers. Need to wrap entire function
+const asyncHandler = require('express-async-handler');
 
-const loginUser = (req, res) => {
+// * @desc Register a new user
+// * @route /api/users
+// * access Public
+const registerUser = asyncHandler(async (req, res) => {
+  // Destructure
+  const { name, email, password } = req.body;
+
+  //Validate
+  if (!name || !email || !password) {
+    // send the client error, client didn't fill out all the info
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
+  console.log(req.body);
+  res.send('Register Route');
+});
+
+// * @desc Login a new user
+// * @route /api/users/login
+// * access Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route');
-};
+});
 
 module.exports = {
   loginUser,
--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,32 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
+// Custom Middleware for status code in routes
+const {errorHandler} = require('./middleware/errorMiddleware');
+
 const app = express();
 // app.get('/', (req, res)=> res.send('Hello'))
 // app.get('/api/users', (req, res) =>
 //   res.status(200).json({ message: 'Welcome to the support desk api' })
 // );
 
-// Routes
+// * MIDDLEWARE HANDLES THE BODY PARSER
+// *-------------------------------------------------------------
+// * TO SEND RAW JSON
+app.use(express.json());
+// * TO SEND URL ENCODED FORM
+app.use(express.urlencoded({ extended: false }));
+// *-------------------------------------------------------------
+
+//* Routes
+// *-------------------------------------------------------------
 // connecting route, point to routes folder
 app.use('/api/users', require('./routes/userRoutes'));
 
-app.listen(PORT, () => console.log(`Server started on ${PORT} Yayyy`));
+// Middleware for status codes
+app.use(errorHandler);
+// *-------------------------------------------------------------
+
+app.listen(PORT, () => console.log(`Server started on ${PORT} Got em`));
+
+
