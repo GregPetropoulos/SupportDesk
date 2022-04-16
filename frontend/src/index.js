676841f587c17aedfa2dfa6643d54c8680e73d2b
GP <gregpetropoulos@yahoo.com>
2022-04-16 11:13:34 -0400

    Added index.css and clean up.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-import ReactDOM from 'react-dom/client';
-  
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// FOR REACT 18 USE THIS BUT NOT UNTIL THEY CATCH UP WITH THE ERRORS MAINTAINED
+// import ReactDOM from 'react-dom/client';
+  
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>
+// );
