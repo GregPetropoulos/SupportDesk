371c6586a04570ef3b62b1bba91c0363bb50f3d0
GP <gregpetropoulos@yahoo.com>
2022-04-22 15:15:31 -0400

    Created a ticketController and ticketRoutes to handle CRUD operations of a single ticket by it's ticket id from the db via mongoose. All CRUD on backend is confirmed functional in MongoDB and in Postman.

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -44,12 +44,104 @@ const createTickets = asyncHandler(async (req, res) => {
     user: req.user.id,
     status: 'new'
   });
-
   // 201 for created
   res.status(201).json(ticket);
 });
 
+// * @desc Get user ticket
+// * @route GET /api/ticket/:id
+// * access Private
+const getTicket = asyncHandler(async (req, res) => {
+  // Get the user by using the id in the JWT
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+  // access ticket id in url by req.params.id
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(401);
+    throw new Error('Ticket not found');
+  }
+
+  // Must be user's ticket
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  res.status(200).json(ticket);
+});
+
+// * @desc DELETE a user ticket
+// * @route DELETE /api/tickets/:id
+// * access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  // Get the user by using the id in the JWT
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+  // access ticket id in url by req.params.id
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(401);
+    throw new Error('Ticket not found');
+  }
+
+  // Must be user's ticket
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+  // Dont need to return anything from this, no variable needed just delete/remove here
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+// * @desc Update a user ticket
+// * @route PUT /api/ticket/:id
+// * access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get the user by using the id in the JWT
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+  // access ticket id in url by req.params.id
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(401);
+    throw new Error('Ticket not found');
+  }
+
+  // Must be user's ticket
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  // This updates the ticket via Ticket model by finding the ticket id in the params and updating the ticket in the db with the the req.body, if anything doesn't exist, the {new:true} will just create it.
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
 module.exports = {
   getTickets,
-  createTickets
+  getTicket,
+  createTickets,
+  updateTicket,
+  deleteTicket,
 };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const {
+  createTickets,
   getTickets,
-  createTickets
+  getTicket,
+  updateTicket,
+  deleteTicket
 } = require('../controllers/ticketController');
 
 const { protect } = require('../middleware/authMiddleware');
 
+//*Gets all auth users tickets and creates a ticket (read, create)
 // can chain on .get and .post to the .route just to get the users tickets
 // In the GET handler the protect is so only an authenticated user can get his own tickets only.
 // Could admin functionality to get all tickets later...
 router.route('/').get(protect, getTickets).post(protect, createTickets);
 
-module.exports = router;
+//*To get an auth user's single ticket by ticket id, read, edit and delete
+router.route('/:id').get(protect, getTicket).put(protect, updateTicket).delete(protect, deleteTicket);
+
+
+module.exports = router
