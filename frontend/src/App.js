3ed70a47fc5eb6356b202a7673bfb4ae2b9faae8
GP <gregpetropoulos@yahoo.com>
2022-04-22 16:12:36 -0400

    On the frontend created a new-ticket page that is protected by a nested route in app.js and includes a custom hook for chekcing if the user is logged in and updating their status.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,30 +2,35 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // In order to make toastify work through out application must bring in ToastContainer AND toastify css file
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
-
-
+import 'react-toastify/dist/ReactToastify.css';
 
 import Header from './components/Header';
 // Pages
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import NewTicket from './pages/NewTicket';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <>
       <Router>
         <div className='container'>
-          <Header/>
+          <Header />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            {/* Nested private route with custom hook------------- */}
+            <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
+            {/* --------------------------------------------------- */}
           </Routes>
         </div>
       </Router>
-      <ToastContainer/>
+      <ToastContainer />
     </>
   );
 }
