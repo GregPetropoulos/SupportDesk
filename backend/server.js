3cde8f61bf90bc50adac8f6a672ccd5501f450b2
GP <gregpetropoulos@yahoo.com>
2022-04-27 17:17:16 -0400

    Set up for deployment to heroku

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // *-------------------------------------------------------------
 
-//* Routes
+//* ROUTES
 // *-------------------------------------------------------------
 // connecting route, point to routes folder
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// *SERVE FRONTEND--DEPLOYMENT TO HEROKU
+if (process.env.NODE_ENV === 'production') {
+  // set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
+}
 
 // Middleware for status codes
 app.use(errorHandler);
