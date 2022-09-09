58923283215d338d625bf3d21067a2ee5cf4eb7e
GP <gregpetropoulos@yahoo.com>
2022-09-09 12:21:00 -0400

    Commented out line 37-51 in server.js to stop the server from serving a static folder, Render platform on the static site service  will npm run render-postbuild to handle the build folder.

--- backend/server.js
+++ backend/server.js
@@ -33,22 +33,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 
-// *SERVE FRONTEND--DEPLOYMENT TO HEROKU
-if (process.env.NODE_ENV === 'production') {
-  // set build folder as static
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+// *SERVE FRONTEND--DEPLOYMENT TO RENDER
+// if (process.env.NODE_ENV === 'production') {
+//   // set build folder as static
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  // app.get('*', (req, res) =>
-  //   res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  // );
-  app.get('*', (_, res) =>
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' });
-  });
-}
+//   // app.get('*', (req, res) =>
+//   //   res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+//   // );
+//   app.get('*', (_, res) =>
+//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+//   );
+// } else {
+//   app.get('/', (req, res) => {
+//     res.status(200).json({ message: 'Welcome to the Support Desk API' });
+//   });
+// }
 
 //*Middleware for status codes
 app.use(errorHandler);
