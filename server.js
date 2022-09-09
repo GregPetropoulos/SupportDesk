277a422eebe624b291f08e779bd791d5f3a54741
GP <gregpetropoulos@yahoo.com>
2022-09-09 15:05:22 -0400

    Restructured folders

--- server.js
+++ server.js
@@ -52,6 +52,13 @@ app.use('/api/tickets', require('./routes/ticketRoutes'));
 //     res.status(200).json({ message: 'Welcome to the Support Desk API' });
 //   });
 // }
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
 
 //*Middleware for status codes
 app.use(errorHandler);
