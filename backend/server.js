c3f67d7818be36907b29e16ff234bb41d543e9a9
GP <gregpetropoulos@yahoo.com>
2022-09-09 14:31:44 -0400

    2nd attempt at commenting out the serve.

--- backend/server.js
+++ backend/server.js
@@ -33,25 +33,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 
-// *SERVE FRONTEND--DEPLOYMENT TO RENDER
-if (process.env.NODE_ENV === 'production') {
-  // set build folder as static
-  // app.use(express.static(path.join(__dirname, '../frontend/build')));
-  // app.use(express.static('../frontend/build'));
-console.log("Hit if statement in server.js,process.env.NODE_ENV === 'production",process.env.NODE_ENV === 'production')
+// // *SERVE FRONTEND--DEPLOYMENT TO RENDER
+// if (process.env.NODE_ENV === 'production') {
+//   // set build folder as static
+//   // app.use(express.static(path.join(__dirname, '../frontend/build')));
+//   // app.use(express.static('../frontend/build'));
+// console.log("Hit if statement in server.js,process.env.NODE_ENV === 'production",process.env.NODE_ENV === 'production')
 
-  // app.get('*', (req, res) =>
-  //   // res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  //   res.sendFile(path.resolve(__dirname, '../','frontend', 'build','index.html'))
-  // );
-  // app.get('*', (_, res) =>
-  //   res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-  // );
-} else {
-  app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' });
-  });
-}
+//   // app.get('*', (req, res) =>
+//   //   // res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+//   //   res.sendFile(path.resolve(__dirname, '../','frontend', 'build','index.html'))
+//   // );
+//   // app.get('*', (_, res) =>
+//   //   res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+//   // );
+// } else {
+//   app.get('/', (req, res) => {
+//     res.status(200).json({ message: 'Welcome to the Support Desk API' });
+//   });
+// }
 
 //*Middleware for status codes
 app.use(errorHandler);
